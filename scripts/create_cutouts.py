4d6b2b1803321a18c5e501ecb5b9429e29164726
afaisst <anfaisst@gmail.com>
2020-07-21 16:04:48 -0700

    Added function in create_cutouts.py to return center of tile for a job file

--- scripts/create_cutouts.py
+++ scripts/create_cutouts.py
@@ -1,3 +1,66 @@
+## Function to get grid point (center and extend + margin) of a tile from a job file
+def get_tile_center_for_job(jobfile_name):
+    '''
+    For a given job file (including path), this function returns the center and width (+margin)
+    of the tile run by the job file
+    '''
+
+    ## Read JSON file
+    with open(jobfile_name,"r") as json_file:
+            job_input = json.load(json_file)
+
+    ## Get necessary information
+    cutoutsize_arcsec = job_input["cutoutsize_arcsec"] # in arcseconds
+    margin_arcsec = job_input["cutoutoverlap_arcsec"] # in arcseconds
+    tile_nbr = job_input["tile_id"]
+    img_large_lr_path = job_input["lr_large_image_name"]
+    
+    ## Read LR image (only header)
+    with fits.open(img_large_lr_path) as hdul:
+        img_large_lr_header = hdul[1].header
+    lr_pixscale = np.abs(img_large_lr_header["CD1_1"]*3600) # pixel scale in arcsec/px
+
+    ## Create grid (centers)
+
+    # get width
+    width_RA = img_large_lr_header["NAXIS1"]*lr_pixscale/60 # in arcminutes
+    width_DEC = img_large_lr_header["NAXIS2"]*lr_pixscale/60 # in arcminutes
+
+    # get number of tiles that fit the low-res image (high-res image has to cover same area or larger)
+    ntiles_RA = int(np.ceil(width_RA / (cutoutsize_arcsec[0]/60))) # width is in arcmin and cutout size in arcsec -> convert
+    ntiles_RA_rest = np.abs(ntiles_RA*cutoutsize_arcsec[0] - width_RA*60) # in arcsec
+    ntiles_DEC = int(np.ceil(width_DEC / (cutoutsize_arcsec[1]/60))) # width is in arcmin and cutout size in arcsec -> convert
+    ntiles_DEC_rest = np.abs(ntiles_DEC*cutoutsize_arcsec[1] - width_DEC*60) # in arcsec
+
+    # get starting point
+    ref_point_lr = wcs.WCS(img_large_lr_header).all_pix2world(0,0,0)
+
+    # get grids for lr (used in the end)
+    grid_lr_ra = np.asarray([ ref_point_lr[0] + (cutoutsize_arcsec[0]/3600)/2 - (cutoutsize_arcsec[0]/3600)*(ii+1) for ii in range(ntiles_RA) ]) + (ntiles_RA_rest/3600)/2
+    grid_lr_dec = np.asarray([ ref_point_lr[1] - (cutoutsize_arcsec[1]/3600)/2 + (cutoutsize_arcsec[1]/3600)*(ii+1) for ii in range(ntiles_DEC) ]) - (ntiles_DEC_rest/3600)/2
+    grid_lr_all = np.meshgrid(grid_lr_ra,grid_lr_dec)
+    grid_lr_all[0] = grid_lr_all[0].flatten()
+    grid_lr_all[1] = grid_lr_all[1].flatten()
+
+
+    grid_lr = [[],[]]
+    grid_lr[0] = np.asarray([grid_lr_all[0][tile_nbr]])
+    grid_lr[1] = np.asarray([grid_lr_all[1][tile_nbr]])
+
+    out = {"ra_center":grid_lr[0][0],
+          "dec_center":grid_lr[1][0],
+           "ra_width_arcsec":cutoutsize_arcsec[0],
+           "dec_width_arcsec":cutoutsize_arcsec[1],
+            "ra_width_margin_arcsec":cutoutsize_arcsec[0]+margin_arcsec[0],
+           "dec_width_margin_arcsec":cutoutsize_arcsec[1]+margin_arcsec[1],
+          }
+
+    #plt.plot(grid_lr_all[0] , grid_lr_all[1] , "o")
+    #plt.plot(out["ra_center"] , out["dec_center"] , "o" , color="red" , markersize=10)
+
+    return(out)
+
+
 
 
 ## Cut out an image slice.
